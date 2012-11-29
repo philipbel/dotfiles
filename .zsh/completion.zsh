d8706a4c279d248466d8f78341d809dfd058aa31
Philip Belemezov <philip@belemezov.net>
2012-11-29 11:44:42 +0200

    [zsh] Show dots when completion is slow

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -31,4 +31,14 @@ zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 # Make rm not offer the same file, if it's already on the cmd line
 zstyle ':completion:*:rm:*' ignore-line yes
 
-
+# See <http://stackoverflow.com/questions/171563/whats-in-your-zshrc>, asnwer
+# from "a paid nerd"
+# On slow infrastructure where tab-completion takes a while? Show "waiting dots"
+# while something tab-completes. (found on zsh-users)
+expand-or-complete-with-dots() {
+  echo -n "\e[31m......\e[0m"
+  zle expand-or-complete
+  zle redisplay
+}
+zle -N expand-or-complete-with-dots
+bindkey "^I" expand-or-complete-with-dots
