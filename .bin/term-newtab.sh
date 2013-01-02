b108c5eadbc7c7eb1650a8e9cdc9910f0b57deef
Philip Belemezov <philip@belemezov.net>
2013-01-02 16:00:52 +0200

    [bin] Support linux ({gnome,mate}-terminal) in term-newtab.sh

--- .bin/term-newtab.sh
+++ .bin/term-newtab.sh
@@ -13,6 +13,62 @@ if [ $# -lt 1 ]; then
 fi
 
 
+linuxTab() {
+    pushd $PWD >/dev/null
+
+    local de=
+    local terminal=
+
+    # see .zsh/alias.zsh
+    if [ -n "$KDE_SESSION_VERSION" ]; then
+        de=kde
+        terminal=konsole
+        echo "$self: KDE not supported"
+        exit 1
+    elif [ "$DESKTOP_SESSION" = "gnome" ]; then
+        de=gnome
+        terminal=gnome-terminal
+    elif [ "$DESKTOP_SESSION" = "mate" ]; then
+        de=mate
+        terminal=mate-terminal
+    elif [ "$DESKTOP_SESSION" = "xfce" ]; then
+        de=xfce
+        terminal=xfce-terminal
+        echo "$self: XFCE not supported"
+        exit 1
+    else
+        echo "$self: Unsupported desktop environment"
+    fi
+
+    case $de in
+        mate|gnome)
+            # See http://superuser.com/questions/94930/gnome-terminal-new-tab-dbus
+
+            if ! which xdotool >/dev/null; then
+                echo "$self: Please install xdotool"
+                exit 1
+            fi
+            if ! which wmctrl >/dev/null; then
+                echo "$self: Please install wmctrl"
+                exit 1
+            fi
+            local wid=`xdotool search --class "$terminal" | head -1`
+            # The following prints an X error, ignore
+            xdotool windowfocus $wid &>/dev/null
+            xdotool key ctrl+shift+t
+            wmctrl -i -a $wid
+            sleep 0.2 # Give the shell time to initialize
+
+            xdotool type "$*"
+            xdotool key Return
+            ;;
+        *)
+            echo "$self: Unknown desktop environment"
+            exit 1
+    esac
+    popd >/dev/null
+ }
+
 
 newtab() {
     PLATFORM=`uname -s`
@@ -28,6 +84,9 @@ newtab() {
                 end
 EOF
             return $?
+            ;;
+        Linux)
+            linuxTab $*
             ;;
         *)
             echo "$self: Unsupported platform \`$PLATFORM'"
