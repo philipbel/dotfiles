10d9f95a47560c8209da6d5961ac97465bc0e74b
Philip Belemezov <philip@belemezov.net>
2016-03-03 18:39:49 +0200

    [Emacs] Minor Changes to custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,8 +4,43 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(LaTeX-command "latex -synctex=1")
- '(custom-safe-themes (quote ("d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default)))
- '(safe-local-variable-values (quote ((eval setq compile-command (concat "make -k -C " (locate-dominating-file buffer-file-name ".dir-locals.el") " install")) (TeX-close-quote . "\"'") (TeX-open-quote . "\"`") (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t) (c++-default-compiler-options . "-g -Wall -Werror -std=c++11") (c++-default-compiler-options . -std=c++11) (safe-local-variable-values))))
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi has-no-display-manager)
+      "dvi2tty")
+     ((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "xdg-open")
+     (output-html "xdg-open"))))
+ '(custom-safe-themes
+   (quote
+    ("d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default)))
+ '(safe-local-variable-values
+   (quote
+    ((eval setq compile-command
+           (concat "make -k -C "
+                   (locate-dominating-file buffer-file-name ".dir-locals.el")
+                   " install"))
+     (TeX-close-quote . "\"'")
+     (TeX-open-quote . "\"`")
+     (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
+           (add-hook
+            (quote write-contents-functions)
+            (lambda nil
+              (delete-trailing-whitespace)
+              nil))
+           (require
+            (quote whitespace))
+           "Sometimes the mode needs to be toggled off and on."
+           (whitespace-mode 0)
+           (whitespace-mode 1))
+     (whitespace-line-column . 80)
+     (whitespace-style face trailing lines-tail)
+     (require-final-newline . t)
+     (c++-default-compiler-options . "-g -Wall -Werror -std=c++11")
+     (c++-default-compiler-options . -std=c++11)
+     (safe-local-variable-values))))
  '(vc-follow-symlinks t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
